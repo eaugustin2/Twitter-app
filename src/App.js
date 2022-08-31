6378043ecb72aa502bc19f845dfc503553b4e34a
Elisee Augustin <elisee@Elisees-MBP.home>
2022-08-31 08:40:31 -0400

    added a navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 
 const App = () => {
+
   return (
     <div className="App">
       <NavBar />
-      <h1>Hello World!</h1>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Nav from "react-bootstrap/Nav";
 
 export const NavBar = () => {
     return(
         <div>
-            <h1>Nav bar placement</h1>
+            <Nav>
+                <Nav.Item>
+                    <Nav.Link href="#">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="#">Moments</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="#">Notifications</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="#">Messages</Nav.Link>
+                </Nav.Item>
+            </Nav>
         </div>
     )
 }
