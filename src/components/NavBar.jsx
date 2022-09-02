34811fb37b6d010bc678ceb7a2e247fa9d6f2683
Elisee Augustin <elisee@elisees-mbp.mynetworksettings.com>
2022-09-01 22:03:39 -0400

    created NavBarItem Component, imported row and col from bootstrap and placed inside NavBar Component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+
+const NavBarItem = ({title, link}) => {
+
+    return(
+        <div>
+            <span>
+                <a href={link}>{title}</a>
+            </span>
+        </div>
+    )
+}
 
 export const NavBar = () => {
     return(
         <div>
-            <Nav>
+            {/* <Nav>
                 <Nav.Item>
                     <Nav.Link href="#">Home</Nav.Link>
                 </Nav.Item>
@@ -17,7 +31,35 @@ export const NavBar = () => {
                 <Nav.Item>
                     <Nav.Link href="#">Messages</Nav.Link>
                 </Nav.Item>
-            </Nav>
+            </Nav> */}
+
+            <Row>
+                <Col>
+                    <NavBarItem title={"Home"} link={"#"} />
+                </Col>
+
+                <Col>
+                    <NavBarItem title={"Moments"} link={"#"} />
+                </Col>
+
+                <Col>
+                    <NavBarItem title={"Notifications"} link={"#"} />
+                </Col>
+
+                <Col>
+                    <NavBarItem title={"Messages"} link={"#"} />
+                </Col>
+            </Row>
+
+            {/* <div className='justify-content-left'>
+                <NavBarItem title={"Home"} link={"#"} />
+
+                <NavBarItem title={"Moments"} link={"#"} />
+
+                <NavBarItem title={"Notifications"} link={"#"} />
+
+                <NavBarItem title={"Messages"} link={"#"} />
+            </div> */}
         </div>
     )
 }
